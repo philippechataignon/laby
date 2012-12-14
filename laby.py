d1b35b84370ce6fd78f4b3adbc2a246aa28eddc8
philippe@micro24 <philippe@chataignon.com>
2012-12-14 22:53:24 +0100

    Version qui dessine le labyrinthe

--- laby.py
+++ laby.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
+import random
+
 
 class Laby(object) :
     def __init__(self, nb_lig, nb_col) :
@@ -9,8 +11,9 @@ class Laby(object) :
         self.nb_col = nb_col
         self.max_seq = nb_lig * nb_col
         self.grille = {(r, c): self.to_seq(r, c) for r in range(nb_lig) for c in range(nb_col)}
-        self.murs_v = {(r, c): True for r in range(nb_lig) for c in range(nb_col+1)}
-        self.murs_h = {(r, c): True for r in range(nb_lig+1) for c in range(nb_col)}
+        self.murs_v = {(r, c): 2 if c==0 or c==nb_col else 1 for r in range(nb_lig) for c in range(nb_col+1)}
+        self.murs_h = {(r, c): 2 if r==0 or r==nb_lig else 1 for r in range(nb_lig+1) for c in range(nb_col)}
+        self.del_murs = 0
 
     def to_seq(self, r, c) :
         return c + self.nb_col * r + 1 
@@ -18,23 +21,52 @@ class Laby(object) :
     def from_seq(self, n) :
         return (n-1) / self.nb_col, (n-1) % self.nb_col
 
-    def affiche_seq(self) :
-        for l in range(self.nb_lig) :
-            for c in range(self.nb_col) :
-                print("%03d" % self.grille.get((l, c), 0), end="")
-            print()
-
-    def affiche(self) :
+    def affiche(self, seq=True) :
         for l in range(self.nb_lig+1) :
             for c in range(self.nb_col) :
-                print(u' \u2014' if self.murs_h[l, c] else '  ', end="")
+                print(u'\u2014'*5 if self.murs_h[l, c] else ' '*5, end="")
             print()
             if l >= self.nb_lig :
                 break
-            for c in range(self.nb_col+1) :
-                print('| ' if self.murs_v[l, c] else '  ', end="")
+            for c in range(self.nb_col) :
+                if seq :
+                    print(('|' if self.murs_v[l, c] else ' ') + ' %2d ' % self.grille[l, c], end="")
+                else :
+                    print(('|' if self.murs_v[l, c] else ' ') + '    ', end="")
+            print('|' if self.murs_v[l, self.nb_col] else '', end="")
             print()
-l = Laby(4, 5)
-l.affiche()
 
-# print l.from_seq(13)
+    def grille_change(self, fro, to) :
+        for key, val in self.grille.iteritems() :
+            if val == fro :
+                self.grille[key] = to
+
+    def mur_alea(self) :
+        v = random.randint(0, 1)
+        l = random.randrange(self.nb_lig+1)
+        c = random.randrange(self.nb_col+1)
+        print(v,l,c)
+
+        if v and self.murs_v.get((l, c)) == 1 :
+            c1 = self.grille[l, c-1]
+            c2 = self.grille[l, c]
+            if c1 != c2 :
+                self.murs_v[(l, c)] = 0
+                self.grille_change(c1, c2)
+                self.del_murs += 1
+        elif not v and self.murs_h.get((l, c)) == 1 :
+            c1 = self.grille[l-1, c]
+            c2 = self.grille[l, c]
+            if c1 != c2 :
+                self.murs_h[(l, c)] = 0
+                self.grille_change(c1, c2)
+                self.del_murs += 1
+
+    def make(self) :
+        while self.del_murs <  self.nb_col * self.nb_lig - 1:
+            l.mur_alea()
+            print(self.del_murs)
+        l.affiche(seq=False)
+
+l = Laby(30, 40)
+l.make()
