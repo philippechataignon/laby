56ae6e929287569edf13e608b46561d32666f0b2
philippe@micro24 <philippe@chataignon.com>
2012-12-15 10:55:58 +0100

    Optimisation des murs
    * Remplace par bool
    * Change random
    * Enleve get suite nouveau random
    * Allège affiche
    * Affiche utilise des listes

--- laby.py
+++ laby.py
@@ -12,8 +12,8 @@ class Laby(object) :
         self.nb_col = nb_col
         self.max_seq = nb_lig * nb_col
         self.grille = {(r, c): self.to_seq(r, c) for r in range(nb_lig) for c in range(nb_col)}
-        self.murs_v = {(r, c): 2 if c==0 or c==nb_col else 1 for r in range(nb_lig) for c in range(nb_col+1)}
-        self.murs_h = {(r, c): 2 if r==0 or r==nb_lig else 1 for r in range(nb_lig+1) for c in range(nb_col)}
+        self.murs_v = {(r, c): True for r in range(nb_lig) for c in range(1, nb_col)}
+        self.murs_h = {(r, c): True for r in range(1, nb_lig) for c in range(nb_col)}
         self.del_murs = 0
         self.chemin = []
 
@@ -24,20 +24,20 @@ class Laby(object) :
         return (n-1) / self.nb_col, (n-1) % self.nb_col
 
     def affiche(self, seq=True) :
+        bh = u'\u2014'
         scale = 6
-        for l in range(self.nb_lig+1) :
-            for c in range(self.nb_col) :
-                print(u'\u2014'*scale if self.murs_h[l, c] else ' '*scale, end="")
-            print()
-            if l >= self.nb_lig :
-                break
-            for c in range(self.nb_col) :
-                if seq :
-                    print(('|' if self.murs_v[l, c] else ' ') + ' %3s ' % self.grille[l, c], end="")
-                else :
-                    print(('|' if self.murs_v[l, c] else ' ') + '    ', end="")
-            print('|' if self.murs_v[l, self.nb_col] else '', end="")
-            print()
+        for l in range(self.nb_lig) :
+            if l == 0 :
+                s_mursh = bh * ( scale * self.nb_col + 1)
+            else :
+                mursh = [bh*scale if self.murs_h[l, c] else ' ' * scale for c in range(self.nb_col)]
+                mursh.append(bh)
+                s_mursh = "".join(mursh)
+            mursv = [('|' if c==0 or self.murs_v[l, c] else ' ') + ' %3s ' % self.grille[l, c] for c in range(self.nb_col)]
+            mursv.append('|')
+            print(s_mursh)
+            print("".join(mursv))
+        print(bh * (scale * self.nb_col + 1))
 
     def grille_change(self, fr, to) :
         for key, val in self.grille.iteritems() :
@@ -46,23 +46,27 @@ class Laby(object) :
 
     def mur_alea(self) :
         v = random.randint(0, 1)
-        l = random.randrange(self.nb_lig+1)
-        c = random.randrange(self.nb_col+1)
+        if v :
+            l = random.randrange(self.nb_lig)
+            c = random.randrange(1, self.nb_col)
+        else :
+            l = random.randrange(1, self.nb_lig)
+            c = random.randrange(self.nb_col)
 
-        if v and self.murs_v.get((l, c)) == 1 :
+        if v and self.murs_v[l, c] :
             c1 = self.grille[l, c-1]
             c2 = self.grille[l, c]
             if c1 != c2 :
                 self.chemin.append((self.to_seq(l, c-1), self.to_seq(l, c)))
-                self.murs_v[(l, c)] = 0
+                self.murs_v[l, c] = False
                 self.grille_change(c1, c2)
                 self.del_murs += 1
-        elif not v and self.murs_h.get((l, c)) == 1 :
+        elif not v and self.murs_h[l, c] :
             c1 = self.grille[l-1, c]
             c2 = self.grille[l, c]
             if c1 != c2 :
                 self.chemin.append((self.to_seq(l-1, c), self.to_seq(l, c)))
-                self.murs_h[(l, c)] = 0
+                self.murs_h[l, c] = False
                 self.grille_change(c1, c2)
                 self.del_murs += 1
 
